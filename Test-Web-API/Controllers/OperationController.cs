45239b09e63fd5f488f4dfd895dfd65b84a860ee
bragr05 <bragrji@gmail.com>
2024-02-26 06:47:00 -0600

    Examples processing http requests

---
+++ Test-Web-API/Controllers/OperationController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Test_Web_API.Controllers
+{
+    // URL of the driver, by default it has a 'helper' which indicates that the driver name
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperationController : ControllerBase
+    {
+        // You must indicate which HTTP method you will use
+        [HttpGet]
+        public decimal Add(decimal a, decimal b) { return a + b; }
+
+        // Example to get params URL and Headers and custom header
+        [HttpPost]
+        public decimal Subtract(Numbers numbers, [FromHeader] string Host, [FromHeader(Name = "Content-Length")] string contentLength, [FromHeader(Name = "X-Custom-Header")] string CustomHeader) 
+        {
+            Console.WriteLine(Host);
+            Console.WriteLine(contentLength);
+            Console.WriteLine(CustomHeader);
+            return numbers.a - numbers.b;
+        }
+    }
+
+    public class Numbers
+    {
+        public decimal a { get; set; }
+        public decimal b { get; set; }
+    }
+}
+
+
---
+++ Test-Web-API/Controllers/PeopleController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Test_Web_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        // Other custom for for access this endpoint
+        [HttpGet("all")]
+        // Especific return type of endpoint (example List<>)
+        public List<People> GetPeoples() => Repository.PeopleList;
+    }
+
+    public class Repository
+    {
+        public static List<People> PeopleList = new List<People>
+        {
+            new People()
+            {
+                Id = 1,
+                Name = "Foo",
+                Birthdate = DateTime.Today,
+            },
+            new People()
+            {
+                Id=2,
+                Name = "Bar",
+                Birthdate = DateTime.Today,
+            },
+            new People()
+            {
+                Id=3,
+                Name = "Yee",
+                Birthdate = DateTime.Today,
+            }
+        };
+    }
+    public class People
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthdate { get; set; }
+    }
+}
